8185ebf781667eca6e24516a4dfeaec0c11fe7e1
fu-taku <age.of.pan+github@gmail.com>
2017-12-01 11:26:05 +0900

    Replace yahoo to google

--- scraper.py
+++ scraper.py
@@ -14,12 +14,19 @@ info_logger.addHandler(stream_handler)
 
 
 def handler(event, context):
-    res = requests.get('http://www.yahoo.co.jp/', timeout=10, verify=False)
+    res = requests.get('https://google.co.jp/', timeout=10)
+
     content_type_encoding = res.encoding if res.encoding != 'ISO-8859-1' else None
-    info_logger.info('page title is "{}".'.format(get_page_title(res, content_type_encoding)))
+    soup = BeautifulSoup(res.content, 'html.parser', from_encoding=content_type_encoding)
+
+    info_logger.info('page title is "{}".'.format(get_page_title(soup)))
+    info_logger.info('antenna for everyone is "{}".'.format(get_footer_bar(soup)))
     return res.status_code
 
 
-def get_page_title(res, encoding):
-    soup = BeautifulSoup(res.content, 'html.parser', from_encoding=encoding)
-    return soup.select("title")[0].string
+def get_page_title(soup):
+    return soup.title.string
+
+
+def get_footer_bar(soup):
+    return soup.find('a', id='gb_70').string
